8e8179b1f875848786e57171ae44187447a20e71
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-16 20:28:34 -0300

    s

--- dados.sql
+++ dados.sql
@@ -49,5 +49,77 @@ select distinct(vendedor) from vendas;
 -- Liste o nome do produto e valor vendido
 select produto, preco_unitario from venda
 
+create database lojaDB;
+use lojaDB;
+
+-- criar tabela de vendas
+create table vendas (
+id int primary key auto_increment,
+produto varchar (100),
+categoria varchar(50),
+quantidade int,
+preco_unitario DECIMAL(10,2),
+vendedor varchar(100),
+data_venda date
+);
+-- VISUALIZAR REGISTROS EM VENDA
+select * from vendas;
+
+-- INSERIR OS REGISTROS
+insert into vendas (produto, categoria, 
+quantidade, preco_unitario, vendedor,
+data_venda) values
+('Notebook Dell', 'Informatica', 2,3500.00,
+'Ana', '2025-09-01'),
+('Mouse Gamer', 'Informatica', 5, 150.00,
+'Carlos', '2025-09-02'),
+('Teclado Mecânico', 'Informatica', 3, 400.00,
+'Ana', '2025-09-02'),
+('Cadeira Gamer', 'Móveis', 1, 1200.00,
+'Beatriz', '2025-09-03'),
+('Mesa de Escritório', 'Móveis', 2, 2200.00,
+'Carlos', '2025-09-04'),
+('Smartphone', 'Eletrônico', 4, 2200.00,
+'Ana', '2025-09-05'),
+('Smartphone Iphone', 'Eletrônico', 2, 6500.00,
+'Beatriz', '2025-09-05'),
+('TV LG 55', 'Eletrônico', 1, 3800.00,
+'Carlos', '2025-09-06'),
+('Fone Bluetooth', 'Eletrônico', 6, 300.00,
+'Ana', '2025-09-06'),
+('Impressora HP', 'Informatica', 6, 300.00,
+'Beatriz', '2025-09-07');
+
+
+-- selecione todos os registros da tabela vendas
+select * from vendas;
+
+-- Liste os vendedores de forma única
+select distinct(vendedor) from vendas;
+
+-- Liste o nome do produto e valor vendido
+select produto, 
+preco_unitario*quantidade as vl_vendido  
+from vendas;
+
+-- quais os produtos vendidos por ana
+select produto from vendas 
+where vendedor = 'Ana';
+
+-- selecione todos os produtos da categoria eletrônicos
+select produto from vendas
+where categoria = 'Eletrônico'
+
+-- Conte quantos registros existe na tabela vendas
+select count(produto) from vendas;
+
+-- Quanto foi vendido em cada categoria
+select categoria,
+sum(preco_unitario*quantidade) as vl_venda 
+from vendas
+group by categoria
+order by vl_venda desc;
+
+
 
 
