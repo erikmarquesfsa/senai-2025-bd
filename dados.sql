bc702acc9b0147afba6181f7631fafa3becf5a27
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-16 19:48:15 -0300

    d

--- dados.sql
+++ dados.sql
@@ -19,3 +19,23 @@ quantidade, preco_unitario, vendedor,
 data_venda) values
 ('Notebook Dell', 'Informatica', 2,3500.00,
 'Ana', '2025-09-01'),
+('Mouse Gamer', 'Informatica', 5, 150.00,
+'Carlos', '2025-09-02'),
+('Teclado Mecânico', 'Informatica', 3, 400.00,
+'Ana', '2025-09-02'),
+('Cadeira Gamer', 'Móveis', 1, 1200.00,
+'Beatriz', '2025-09-03'),
+('Mesa de Escritório', 'Móveis', 2, 2200.00,
+'Carlos', '2025-09-04'),
+('Smartphone', 'Eletrônico', 4, 2200.00,
+'Ana', '2025-09-05'),
+('Smartphone Iphone', 'Eletrônico', 2, 6500.00,
+'Beatriz', '2025-09-05'),
+('TV LG 55', 'Eletrônico', 1, 3800.00,
+'Carlos', '2025-09-06'),
+('Fone Bluetooth', 'Eletrônico', 6, 300.00,
+'Ana', '2025-09-06'),
+('Impressora HP', 'Informatica', 6, 300.00,
+'Beatriz', '2025-09-07');
+
+
