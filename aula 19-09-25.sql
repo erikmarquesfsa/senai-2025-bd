ad783736cb98b21de9fc9c451677e6ad5dcb643c
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-19 19:45:26 -0300

    d

--- aula 19-09-25.sql
+++ aula 19-09-25.sql
@@ -34,3 +34,17 @@ insert into Aluno(nome, data_nascimento,curso) values
 ('Carlos Silva','1999-11-23','Direito'),
 ('Mariana Lima','2001-02-17','Medicina'),
 ('João Pedro','2000-08-09','Engenharia');
+
+insert into Professor (nome, disciplina) values
+('Ricardo Alves','Matemática'),
+('Fernando Rocha','Direito Constitucional'),
+('Paulo Mendes','Anatomia');
+
+insert into Matricula (idAluno, idProfessor, semestre) values
+(1,1,'2024-1'),
+(2,2,'2024-1'),
+(3,3,'2024-1'),
+(4,1,'2024-1'),
+(1,3,'2024-2');
+
+
