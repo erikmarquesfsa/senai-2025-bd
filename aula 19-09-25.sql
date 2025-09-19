188f5b6e7d6c246412385408142956537d9cb8d3
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-19 19:14:11 -0300

    d

--- aula 19-09-25.sql
+++ aula 19-09-25.sql
@@ -6,4 +6,13 @@ idAluno int auto_increment primary key,
 nome varchar(100),
 data_nascimento DATE,
 curso varchar (100)
-)
+);
+
+select * from Aluno;
+
+create table Professor(
+idProfessor int auto_increment primary key,
+nome varchar(100),
+disciplina varchar(100)
+);
+select * from professor;
