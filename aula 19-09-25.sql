81ff87d7c9e932bdc08a4182575427842cd83842
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-19 19:34:59 -0300

    f

--- aula 19-09-25.sql
+++ aula 19-09-25.sql
@@ -16,3 +16,15 @@ nome varchar(100),
 disciplina varchar(100)
 );
 select * from professor;
+
+-- tabela de matricula
+-- ligação entre o aluno e o professor
+
+create table matricula(
+idMatricula int auto_increment primary key,
+idAluno int,
+idProfessor int,
+semestre varchar(10),
+foreign key (idAluno) references Aluno(idAluno),
+foreign key (idProfessor) references Professor(idProfessor)
+);
