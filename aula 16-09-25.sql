ca38fb205e4880e7f5cfd794549266b05f5688de
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-09-19 21:10:12 -0300

    Final aula Banco

--- aula 16-09-25.sql
+++ aula 16-09-25.sql
@@ -1,85 +1,106 @@
-create database lojaDB;
-use lojaDB;
+create database EscolaDB;
+use EscolaDB;
 
--- criar tabela de vendas
-create table vendas (
-id int primary key auto_increment,
-produto varchar (100),
-categoria varchar(50),
-quantidade int,
-preco_unitario DECIMAL(10,2),
-vendedor varchar(100),
-data_venda date
+create table Aluno (
+idAluno int auto_increment primary key,
+nome varchar(100),
+data_nascimento DATE,
+curso varchar (100)
 );
--- VISUALIZAR REGISTROS EM VENDA
-select * from vendas;
 
--- INSERIR OS REGISTROS
-insert into vendas (produto, categoria, 
-quantidade, preco_unitario, vendedor,
-data_venda) values
-('Notebook Dell', 'Informatica', 2,3500.00,
-'Ana', '2025-09-01'),
-('Mouse Gamer', 'Informatica', 5, 150.00,
-'Carlos', '2025-09-02'),
-('Teclado Mecânico', 'Informatica', 3, 400.00,
-'Ana', '2025-09-02'),
-('Cadeira Gamer', 'Móveis', 1, 1200.00,
-'Beatriz', '2025-09-03'),
-('Mesa de Escritório', 'Móveis', 2, 2200.00,
-'Carlos', '2025-09-04'),
-('Smartphone', 'Eletrônico', 4, 2200.00,
-'Ana', '2025-09-05'),
-('Smartphone Iphone', 'Eletrônico', 2, 6500.00,
-'Beatriz', '2025-09-05'),
-('TV LG 55', 'Eletrônico', 1, 3800.00,
-'Carlos', '2025-09-06'),
-('Fone Bluetooth', 'Eletrônico', 6, 300.00,
-'Ana', '2025-09-06'),
-('Impressora HP', 'Informatica', 6, 300.00,
-'Beatriz', '2025-09-07');
+select * from Aluno;
+
+create table Professor(
+idProfessor int auto_increment primary key,
+nome varchar(100),
+disciplina varchar(100)
+);
+select * from professor;
+
+-- tabela de matricula
+-- ligação entre o aluno e o professor
+
+create table matricula(
+idMatricula int auto_increment primary key,
+idAluno int,
+idProfessor int,
+semestre varchar(10),
+foreign key (idAluno) references Aluno(idAluno),
+foreign key (idProfessor) references Professor(idProfessor)
+);
+
+insert into Aluno(nome, data_nascimento,curso) values 
+('Ana Souza','2000-05-12','Engenharia'),
+('Carlos Silva','1999-11-23','Direito'),
+('Mariana Lima','2001-02-17','Medicina'),
+('João Pedro','2000-08-09','Engenharia');
+
+insert into Professor (nome, disciplina) values
+('Ricardo Alves','Matemática'),
+('Fernando Rocha','Direito Constitucional'),
+('Paulo Mendes','Anatomia');
+
+insert into Matricula (idAluno, idProfessor, semestre) values
+(1,1,'2024-1'),
+(2,2,'2024-1'),
+(3,3,'2024-1'),
+(4,1,'2024-1'),
+(1,3,'2024-2');
+
+select * from aluno;
+select * from professor;
+select * from matricula;
+
+-- QUESTÕES
+-- lISTAR TODOS OS ALUNOS CADASTRADOS
+select * from aluno;
+
+-- Mostre apenas o nome e o curso dos alunos
+select nome, curso from aluno;
+
+-- listar todos os professores e suas disciplinas
+select nome,disciplina from professor;
+
+-- mostre todas as matriculas realizadas no semestre 2024-1
+select * from matricula where semestre = '2024-1';
+
+-- exiba o nome do aluno e do professor de todas as matricula
+select a.nome,p.nome
+from matricula as m
+join aluno as a on m.idaluno = a.idAluno
+join professor as p on m.idprofessor= p.idprofessor
+
+-- conte quantos alunos estão matriculados em cada professor
+select p.nome,count(m.idAluno) as total
+from matricula as m
+join professor as p on m.idprofessor= p.idprofessor
+group by p.nome
+
+-- mostre quais alunos fazem engenharia
+select nome 
+from aluno
+where curso = 'Engenharia';
+
+-- liste todos os professores que possuem alunos matriculados
+select distinct(p.nome)
+from matricula m
+join professor as p on m.idprofessor= p.idprofessor;
+
+-- mostre a quantidade de alunos matriculados por semestre
+select semestre, count(idaluno) as T
+from matricula
+group by semestre;
+
+/* exiba o nome do professor que ministra a disciplina 
+  de anatomia e o nome dos seus alunos */
+
+SELECT p.nome as Professor, a.nome as Aluno
+from matricula as M
+join aluno as a on m.idaluno = a.idAluno
+join professor as p on m.idprofessor= p.idprofessor
+where p.disciplina = 'Anatomia';
 
 
--- selecione todos os registros da tabela vendas
-select * from vendas;
 
--- Liste os vendedores de forma única
-select distinct(vendedor) from vendas;
 
--- Liste o nome do produto e valor vendido
-select produto, 
-preco_unitario*quantidade as vl_vendido  
-from vendas;
-
--- quais os produtos vendidos por ana
-select produto from vendas 
-where vendedor = 'Ana';
-
--- selecione todos os produtos da categoria eletrônicos
-select produto from vendas
-where categoria = 'Eletrônico'
-
--- Conte quantos registros existe na tabela vendas
-select count(produto) from vendas;
-
--- Quanto foi vendido em cada categoria
-select categoria,
-sum(preco_unitario*quantidade) as vl_venda 
-from vendas
-group by categoria
-order by vl_venda desc;
-
--- quanto cada vendedor vendeu 
-select 
-vendedor,
-sum(preco_unitario*quantidade)
-from vendas
-group by vendedor;
-
--- quanto foi a média vendida por dia
-select data_venda,categoria,
-SUM(preco_unitario*quantidade) as vl_vendido,
-avg(preco_unitario*quantidade) as md_vendida
-from vendas
-group by categoria,data_venda;
 
